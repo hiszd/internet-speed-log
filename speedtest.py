6a5b80b50f196f67a67197a9423d7f915d89529f
Zion Koyl <hiszd1@gmail.com>
2022-08-09 10:57:21 -0400

    Added ping to the log file

--- speedtest.py
+++ speedtest.py
@@ -10,6 +10,12 @@ stats = cmd.run('speedtest-cli')
 # Download: 38.87 Mbits/s
 # Upload: 5.29 Mbits/s
 
+ping = re.compile( r']: ([0-9]*\.[0-9]*.*)\n').findall(stats)
+if len(ping) > 0:
+    ping = ping[0]
+else:
+    ping = "ERROR"
+
 download = re.compile( r'Download: ([0-9]*\.[0-9]*.*)\n').findall(stats)
 if len(download) > 0:
     download = download[0]
@@ -26,8 +32,8 @@ else:
 
 time = timestamp.now()
 
-string = time + ', ' + download + ', ' + upload
+string = time + ', ' + download + ', ' + upload + ', ' + ping
 
 f = open("speedLog.txt", 'a')
-f.write(s + "\n")
+f.write(string + "\n")
 f.close()
